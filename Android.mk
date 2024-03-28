e33cc2a202b4fddc746aa8945ad2184d44b33b2d
Bruno Martins <bgcngm@gmail.com>
2024-03-28 14:12:52 +0000

    sm6375-common: Mark CNE symlink target as required package
    Change-Id: I0bfe65ba61b6aa9916fe6459be2a84d772939a2f

--- Android.mk
+++ Android.mk
@@ -106,14 +106,6 @@ $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) \
                                  $(RFS_MSM_WPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS) $(WIFI_FIRMWARE_SYMLINKS)
 
-CNE_APP_SYMLINKS := $(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64
-$(CNE_APP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating CneApp symlinks: $@"
-	@mkdir -p $@
-	$(hide) ln -sf /vendor/lib64/libvndfwk_detect_jni.qti.so $@/libvndfwk_detect_jni.qti.so
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_APP_SYMLINKS)
-
 AW882XX_CAL_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/aw_cali.bin
 $(AW882XX_CAL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating aw882xx firmware symlinks: $@"
