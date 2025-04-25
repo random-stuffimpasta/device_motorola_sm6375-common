85505835c071d5ed5b1c9962f9da7e3131a8c975
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-04-26 04:53:05 +0900

    sm6375-common: Enable UFFD_GC * Our kernel supports it.

--- common.mk
+++ common.mk
@@ -229,7 +229,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # Kernel
-OVERRIDE_ENABLE_UFFD_GC := false
+PRODUCT_ENABLE_UFFD_GC := true
 
 # Keymaster
 PRODUCT_PACKAGES += \
